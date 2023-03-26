e8e89080cca39577de1e47f642ca28e327d43af4
AayushJain09 <ayushandy404@gmail.com>
2023-03-26 16:08:12 +0530

    create user auth and signup

--- components/User.js
+++ components/User.js
@@ -1,8 +1,23 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+// import Image from "next/image";
 
 export default function User() {
-  return (
-    <div>
-      User
-    </div>
+    const {data: session} = useSession();
+    if(session){
+        return (
+            <>
+                <img 
+                    onClick={signOut} 
+                    src={session.user.image} 
+                    alt="user profile"
+                    className="h-10 w-10 p-1 hover:bg-gray-400 rounded-full cursor-pointer"
+                />
+            </>
+        )
+    }
+    return (
+    <>
+      <button onClick={signIn} className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md' >Sign In</button>
+    </>
   )
 }
